9aafe06b73547c6124d9c352377e66f6881436c4
liushi <liushi1211@hotmail.com>
2017-04-04 20:58:44 +0800

    update code

---
+++ common-jdbc/src/main/java/com/funkyer/jdbc/JdbcTemplate.java
@@ -0,0 +1,34 @@
+package com.funkyer.jdbc;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
+
+/**
+ * Created by dell on 17-4-4.
+ */
+public class JdbcTemplate extends SqlMapClientDaoSupport 
+{
+	
+	public void add(String statementName,Object parameterObject)
+	{
+		this.getSqlMapClientTemplate().insert(statementName, parameterObject);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object> query(String statementName,Map<String,Object> parameterObject)
+	{
+		return this.getSqlMapClientTemplate().queryForList(statementName, parameterObject);
+	}
+	
+	public void update(String statementName,Object parameterObject)
+	{
+		this.getSqlMapClientTemplate().update(statementName, parameterObject);
+	}
+	
+	public void delete(String statementName,Map<String,Object> parameterObject)
+	{
+		this.getSqlMapClientTemplate().delete(statementName, parameterObject);
+	}
+}
