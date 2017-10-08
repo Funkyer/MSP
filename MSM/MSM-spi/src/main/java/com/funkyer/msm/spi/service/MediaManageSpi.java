9a50c664a99659e1a03e60956a472cc9da2a86b1
liushi <liushi1211@hotmail.com>
2017-10-08 16:52:36 +0800

    update

--- MSM/MSM-spi/src/main/java/com/funkyer/msm/spi/service/MediaManageSpi.java
+++ MSM/MSM-spi/src/main/java/com/funkyer/msm/spi/service/MediaManageSpi.java
@@ -1,32 +1,37 @@
 package com.funkyer.msm.spi.service;
 
+import com.funkyer.msm.api.MediaManage;
 import com.funkyer.msm.spi.JdbcTemplate;
 import com.funkyer.msm.spi.common.Contants;
-import com.funkyer.msm.spi.domain.Media;
+import com.funkyer.msm.api.domain.Media;
 
 import java.util.List;
 
 /**
  * Created by liushi on 17/8/25.
  */
-public class MediaManage
+public class MediaManageSpi implements MediaManage
 {
     private JdbcTemplate jdbcTemplate;
 
+    @Override
     public void add(Media media) {
         jdbcTemplate.add(Contants.MediaSQL.MediaInsertSQL, media);
     }
 
+    @Override
     public void delete(Media media)
     {
         jdbcTemplate.delete(Contants.MediaSQL.MediaDeleteSQL, media);
     }
 
+    @Override
     public List<Object> query(Media media)
     {
         return jdbcTemplate.query(Contants.MediaSQL.MediaQuerySQL, media);
     }
 
+    @Override
     public void update(Media media)
     {
         jdbcTemplate.update(Contants.MediaSQL.MediaUpdateSQL,media);
--- MSM/MSM-spi/src/main/java/com/funkyer/msm/spi/service/VodManageSpi.java
+++ MSM/MSM-spi/src/main/java/com/funkyer/msm/spi/service/VodManageSpi.java
@@ -5,7 +5,8 @@ package com.funkyer.msm.spi.service;
 
 import java.util.List;
 
-import com.funkyer.msm.spi.domain.Vod;
+import com.funkyer.msm.api.VodManage;
+import com.funkyer.msm.api.domain.Vod;
 import com.funkyer.msm.spi.JdbcTemplate;
 import com.funkyer.msm.spi.common.Contants;
 
@@ -13,24 +14,27 @@ import com.funkyer.msm.spi.common.Contants;
  * @author dell
  * 
  */
-public class VodManage {
+public class VodManageSpi implements VodManage
+{
 
 	private JdbcTemplate jdbcTemplate;
 
+	@Override
 	public void add(Vod vod) {
 		jdbcTemplate.add(Contants.VodSQL.VodInsertSQL, vod);
 	}
-	
+	@Override
 	public void delete(Vod vod)
 	{
 		jdbcTemplate.delete(Contants.VodSQL.VodDeleteSQL, vod);
 	}
-	
+	@Override
 	public List<Object> query(Vod vod)
 	{
 		return jdbcTemplate.query(Contants.VodSQL.VodQuerySQL, vod);
 	}
 
+	@Override
 	public void update(Vod vod)
 	{
 		jdbcTemplate.update(Contants.VodSQL.VodUpdateSQL,vod);
