9faa11406396920f268725b81109114731278481
liushi <liushi1211@hotmail.com>
2017-10-17 16:04:51 +0800

    update

--- MSS/content/content-spi/src/test/java/com/funkyer/content/test/TestVodManage.java
+++ MSS/content/content-spi/src/test/java/com/funkyer/content/test/TestVodManage.java
@@ -1,32 +1,33 @@
 package com.funkyer.content.test;
 
+import com.alibaba.dubbo.common.utils.NetUtils;
 import com.funkyer.msm.api.VodManage;
 import com.funkyer.msm.api.domain.Vod;
 import org.omg.Messaging.SyncScopeHelper;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
 
 /**
  * Created by liushi on 17/10/9.
  */
-public class TestVodManage
+public class TestVodManage extends BaseTest
 {
+    static TestVodManage testVodManage = new TestVodManage("classpath:/META-INF/conf/test/vod.spring.xml");
     private ClassPathXmlApplicationContext context = null;
-    public void init(String path)
-    {
-        context = new ClassPathXmlApplicationContext(path);
 
-    }
     public TestVodManage(String path) {
-
-        init(path);
+        super(path);
+        init();
     }
 
 
     public void testVodManage()
     {
-       VodManage vodManage = (VodManage) context.getBean("msm.VodManage");
+       VodManage vodManage = (VodManage) getContext().getBean("msm.VodManage");
         Vod vod = new Vod();
         vod.setId("1");
         System.out.println(vodManage.query(vod));
@@ -34,8 +35,21 @@ public class TestVodManage
     }
 
     public static void main(String[] args) throws IOException {
-         TestVodManage testVodManage = new TestVodManage("classpath:/META-INF/conf/test/vod.spring.xml");
 
+//        String host = null;
+//            try {
+//                long s1 = new Date().getTime();
+//                 host = InetAddress.getLocalHost().getHostName();
+//                long s2 = new Date().getTime();
+//                System.out.println(host + " 耗时=" + (s2 - s1) + "毫秒");
+//            } catch (UnknownHostException var28) {
+//            }
+//        System.out.println(host);
+        long s1 = new Date().getTime();
         testVodManage.testVodManage();
+                        long s2 = new Date().getTime();
+
+        System.out.println( " 耗时=" + (s2 - s1) + "毫秒");
+
     }
 }
